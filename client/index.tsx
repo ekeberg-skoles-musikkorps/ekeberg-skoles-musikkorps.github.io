07be81076851f75d8355fd4fc03d577f97b60a6a
Johannes Brodwall <johannes@brodwall.com>
2023-11-14 08:56:29 +0100

    Initial react application

---
+++ client/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { Application } from "./components/application/application";
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render(<Application />);
